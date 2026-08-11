46d2cd8f923aa6de36ade7263a1190c9de1fffef
Raphael Maciel <raphael.barbosamaciel@student.unimelb.edu.au>
2026-08-11 17:46:54 +1000

    Task2: creating summarise_log function and testing

--- Task 2/collector.py
+++ Task 2/collector.py
@@ -63,7 +63,51 @@ def fetch_all_cities(cities):
     else:
         return []
 
+
+
 def update_log(log_path, new_results):
+
+        list_results=[]
+
+        try:
+            with open(log_path, "r", encoding="utf-8") as file:
+                        content = file.read().strip()
+                        if content:
+                            json_content = json.loads(content)
+                            if isinstance(json_content, list):
+                                for item in json_content:
+                                    if isinstance(item, dict) and item.get('city'):
+                                        list_results.append(item)
+
+        except (AttributeError, FileNotFoundError, json.decoder.JSONDecodeError) as file_not_found:
+            print(f"   [warn] update_log(): {file_not_found} ... trying to create the file if the folder is available")
+            # return False              
+
+        for result in new_results:
+            raw_city = result.get('city')
+            result_city = str(raw_city).strip().capitalize() if raw_city else ""
+            result_fetched_at = result.get('fetched_at')
+            result_weather = result.get('weather')
+            
+            if result_city not in ("", None) and result_fetched_at not in ("", None) and result_weather:
+                new_record = dict(result)
+                new_record["city"] = result_city
+                list_results.append(new_record)
+
+        if len(list_results) > 0:
+            try:
+                with open(log_path, "w", encoding="utf-8") as outfile:
+                    json.dump(list_results, outfile, indent=2, ensure_ascii=False) #converting list[dict] to json and adding to the output file
+            except Exception as e:
+                print(f"   [error] update_log(): Error with the file {log_path}: {e}")
+                return False          
+
+            return True     
+        else:
+            return False
+
+def summarise_log(log_path):
+
     list_results=[]
 
     try:
@@ -72,38 +116,56 @@ def update_log(log_path, new_results):
                     if content:
                         json_content = json.loads(content)
                         if isinstance(json_content, list):
-                            for item in json_content:
-                                if isinstance(item, dict) and item.get('city'):
-                                    list_results.append(item)
-
+                            list_results = json_content
+                            
     except (AttributeError, FileNotFoundError, json.decoder.JSONDecodeError) as file_not_found:
-        print(f"   [warn] update_log(): {file_not_found} ... trying to create the file if the folder is available")
-        # return False              
+        print(f"   [Error] summarise_log() File error: {file_not_found}")
+        return {}              
 
-    for result in new_results:
-        raw_city = result.get('city')
-        result_city = str(raw_city).strip().capitalize() if raw_city else ""
-        result_fetched_at = result.get('fetched_at')
-        result_weather = result.get('weather')
-        
-        if result_city not in ("", None) and result_fetched_at not in ("", None) and result_weather:
-            new_record = dict(result)
-            new_record["city"] = result_city
-            list_results.append(new_record)
+    
 
-    if len(list_results) > 0:
+    city_set= set()
+    fetched_at_list=[]
+    temperature_list=[]
+    
+    for result in list_results:
         try:
-            with open(log_path, "w", encoding="utf-8") as outfile:
-                json.dump(list_results, outfile, indent=2, ensure_ascii=False) #converting list[dict] to json and adding to the output file
-        except Exception as e:
-            print(f"   [error] update_log(): Error with the file {log_path}: {e}")
-            return False          
+            if not isinstance(result, dict):
+                continue
+            
+            city = str(result.get("city", "")).strip().capitalize()
+            fetched_at_val = result.get("fetched_at")
+            
+            weather_data = result.get("weather", {})
+            temp_val = weather_data.get("temperature") if isinstance(weather_data, dict) else None
+            temperature = to_float(temp_val)
 
-        return True     
-    else:
-        return False
-        
-        
+            if city and fetched_at_val and temperature is not None:
+                city_set.add(city)
+                fetched_at_list.append(fetched_at_val)
+                temperature_list.append(temperature)
+
+        except Exception as e:
+            print(f"   [warn] summarise_log record skipped: {e}")
+            continue
+                    
+    if city_set and fetched_at_list and temperature_list:
+        return {
+            "total_records" : len(list_results),
+            "cities_tracked" : sorted(list(city_set)),
+            "latest_fetch" : max(fetched_at_list),
+            "avg_temperature" : round(st.mean(temperature_list),2)
+        }
+    
+    return {
+        "total_records": 0,
+        "cities_tracked": [],
+        "latest_fetch": None,
+        "avg_temperature": None
+    }     
+
+
+
 if __name__ == "__main__":
     cities = [ 
         {"name": "Sydney", "lat": -33.87, "lon": 151.21}, 
@@ -113,5 +175,22 @@ if __name__ == "__main__":
         
     ] 
 
-    print(update_log("weather_log.json", fetch_all_cities(cities)))
+    update_log("weather_log.json", fetch_all_cities(cities)) 
 
+    new = [ 
+        {"city": "Sydney", 
+        "fetched_at": "2026-05-24T05:05:52+00:00", 
+        "weather": {"temperature": 18.4, "windspeed": 12.3, 
+                    "winddirection": 180, "weathercode": 3, 
+                    "time": "2026-05-24T14:00"}},
+        {"city": " Sydney", 
+        "fetched_at": "2026-05-24T05:05:52+00:00", 
+        "weather": {"temperature": 18.4, "windspeed": 12.3, 
+                    "winddirection": 180, "weathercode": 3, 
+                    "time": "2026-05-24T14:00"}} 
+    ] 
+
+
+    update_log("weather_log.json", new)
+    summary = summarise_log("weather_log.json")
+    print(summary)
