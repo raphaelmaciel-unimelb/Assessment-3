57c223e25b3be8b920b67bf7e3a27872f40ac72f
Raphael Maciel <raphael.barbosamaciel@student.unimelb.edu.au>
2026-08-14 14:10:11 +1000

    Task3: formatting dates and rendering list of tracked cities

--- Task 3/report.py
+++ Task 3/report.py
@@ -8,6 +8,7 @@
 
 try:
     from datetime import datetime, timezone, UTC
+    from zoneinfo import ZoneInfo
     import weather as weather
     import collector as collector
     import WMO_code as code
@@ -45,18 +46,23 @@ def read_log(log_path):
 
 def generate_report(log_path, output_path):
 
+    # Reading the log entries from the json file
     log_records_list = read_log(log_path)
 
+    # Receiving the Weather summary from the collector function
     summary = collector.summarise_log(log_path)
     
     data_list=[] 
     dates = []
     
+    # Going throught the log records to prepare the data that will presented in the log entries table
     for record in log_records_list:
         code_description = code.get_code_description(record["weather"]["weathercode"])
+        date_convertion = datetime.fromisoformat(record.get('fetched_at')).astimezone(ZoneInfo("Australia/Melbourne"))
+        
         data={
             "city":record.get('city'),
-            "timestamp":record.get('fetched_at'),
+            "timestamp":date_convertion,
             "temperature (°C)":record["weather"]["temperature"],
             "windspeed (km/h)":record["weather"]["windspeed"],
             "weathercode description":code_description
@@ -65,21 +71,30 @@ def generate_report(log_path, output_path):
         dates.append(record.get('fetched_at'))        
         data_list.append(data)
     
+    # Get the current time for the page heading with the report generation timestamp
+    timestamp_now = datetime.now(ZoneInfo("Australia/Melbourne"))
     
+    # Get the oldest record in the log entries for the Date Range field
     date_from = min(
         dates, 
         key=lambda d: datetime.fromisoformat(d).astimezone(timezone.utc)
     )
+    date_from = datetime.fromisoformat(date_from).astimezone(ZoneInfo("Australia/Melbourne"))
+    
+    # Get the most recent record in the log entries for the Date Range field
+    date_to = max(
+        dates, 
+        key=lambda d: datetime.fromisoformat(d).astimezone(timezone.utc)
+    )
+    date_to = datetime.fromisoformat(date_to).astimezone(ZoneInfo("Australia/Melbourne"))
     
     # 1. Automatically extract table headers from the first data dictionary keys
-    log_entries_table_headers = list(data_list[0].keys())
+    log_entries_table_header = list(data_list[0].keys())
     
-    html_header= """
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Weather Report A3.Task3</title>
-            <timestamp>{{timestamp}}</timestamp>
+    # Sorte the log entries table by fetched timestamp
+    data_list = sorted(data_list, key=lambda item: item['timestamp'])
+    
+    html_style= """            
             <style>
                 body {
                     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
@@ -117,11 +132,23 @@ def generate_report(log_path, output_path):
                     border-bottom: 2px solid #34495e;
                 }
             </style>
+        """
+    
+    html_header="""
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Weather Report A3.Task3</title>
+            <timestamp>{{ timestamp_now.strftime('%d-%m-%Y %H:%M:%S') }}</timestamp>
+            {{html_style}}
         </head>
         """
     html_body="""
         <body>
-                <h1>Weather app</h1>
+                <h1>Weather Report</h1>
+                <p> <b>Subject:</b> AI Programming Fundamentals (COMP90100_2026_OT4_UMO_1)</p>
+                <p> <b>Student:</b> Raphael Barbosa Maciel</p>
+                <p> <b>GitHub repo:</b> <a href=https://github.com/raphaelmaciel-unimelb/Assessment-3>https://github.com/raphaelmaciel-unimelb/Assessment-3</a></p>
                 <h2>Summary</h2>
                 <table class="styled-table">
                     <thead>
@@ -137,15 +164,19 @@ def generate_report(log_path, output_path):
                         </tr>
                         <tr>
                             <td>Cities tracked</td>
-                            <td>{{ summary["cities_tracked"] }}</td>
+                            <td>
+                                {% for city in summary["cities_tracked"] %}
+                                    {{ city}}{% if loop.nextitem %}, {% endif %}
+                                {% endfor %}
+                            </td>
                         </tr>
                         <tr>
                             <td>Date from</td>
-                            <td>{{ date_from }}</td>
+                            <td>{{ date_from.strftime('%d-%m-%Y %H:%M:%S') }}</td>
                         </tr>                            
                         <tr>
                             <td>Date to</td>
-                            <td>{{ summary["latest_fetch"] }}</td>
+                            <td>{{ date_to.strftime('%d-%m-%Y %H:%M:%S') }}</td>
                         </tr>                                                                
                     </tbody>
                 </table>
@@ -154,8 +185,8 @@ def generate_report(log_path, output_path):
                     <thead>
                         <tr>
                             {# Dynamic Header Generation Loop #}
-                            {% for header in headers %}
-                            <th>{{ header.title() }}</th>
+                            {% for entry in entries_table_header %}
+                                <th>{{ entry.title() }}</th>
                             {% endfor %}
                         </tr>
                     </thead>
@@ -163,8 +194,13 @@ def generate_report(log_path, output_path):
                         {# Dynamic Row Generation Loop #}
                         {% for row in data_list %}
                         <tr>
-                            {% for header in headers %}
-                            <td>{{ row[header] }}</td>
+                            {% for entry in entries_table_header %}
+                                {% if entry == 'timestamp'%}    
+                                    <td>{{ row[entry].strftime('%d-%m-%Y %H:%M:%S') }}</td>
+                                {% else %}
+                                    <td>{{ row[entry] }}</td>
+                                {% endif %}
+                            
                             {% endfor %}
                         </tr>
                         {% endfor %}
@@ -185,13 +221,16 @@ def generate_report(log_path, output_path):
     # 3. Process the data using the template engine
     template = Template(html_template_string)
     
+    # Using jinja2 to parse and render the HTML file
     rendered_html = template.render(
-        timestamp=collector.fetched_at(),
+        timestamp_now=timestamp_now,
+        html_style=html_style,
         summary=summary,
         date_from=date_from,
-        headers=log_entries_table_headers, 
+        date_to=date_to,
+        entries_table_header=log_entries_table_header, 
         data_list=data_list
-        )
+    )
     
     # Render to string and save
     with open(output_path, "w", encoding="utf-8") as file:
